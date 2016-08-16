d846997526c1a7af7f05b20ccdfc7f3f662b6495
shibme <shibly.meeran@gmail.com>
2016-08-16 22:51:10 +0530

    Moving AnalyticsBot in

--- src/me/shib/java/lib/jbotstats/BotStatsConfig.java
+++ src/me/shib/java/lib/jbotstats/BotStatsConfig.java
@@ -5,27 +5,25 @@ import java.util.Map;
 
 public final class BotStatsConfig {
 
-    private String botStatsClassName;
+    private Class<JBotStats> botStatsClass;
     private String token;
     private Map<String, String> botStatsConstants;
 
     public BotStatsConfig(Class<JBotStats> botStatsClass, String token) {
-        this.botStatsClassName = botStatsClass.getName();
+        this.botStatsClass = botStatsClass;
         this.token = token;
+        this.botStatsConstants = new HashMap<>();
     }
 
     public String getToken() {
         return token;
     }
 
-    public String getBotStatsClassName() {
-        return botStatsClassName;
+    public Class<JBotStats> getBotStatsClass() {
+        return botStatsClass;
     }
 
     public void setConfig(String key, String value) {
-        if (botStatsConstants == null) {
-            botStatsConstants = new HashMap<>();
-        }
         botStatsConstants.put(key, value);
     }
 
